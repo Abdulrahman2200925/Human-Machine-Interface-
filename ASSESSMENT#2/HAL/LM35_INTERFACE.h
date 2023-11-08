c7ace979c29f5965195fef088161ed4c3a04fd9f
Abdulrahman2200925 <114488000+Abdulrahman2200925@users.noreply.github.com>
2023-11-09 01:54:44 +0200

    Update LM35_INTERFACE.h

--- ASSESSMENT#2/HAL/LM35_INTERFACE.h
+++ ASSESSMENT#2/HAL/LM35_INTERFACE.h
@@ -14,8 +14,7 @@
 u8 ADCToTemperature(u16 adcReading) ;
 void DisplayTemperatureOnLCD(u8 temperature) ;
 void LM35_temp(u16 ADC_READING);
-u16 asciiToU16(const char *str);
-void u16ToAscii(u16 value, char *str);
+
 
 
 

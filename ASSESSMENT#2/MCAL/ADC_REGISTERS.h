b818fcc8e6ecff9014c4ec4e3cde689a5437780b
Kalmasry <114658809+Kalmasry@users.noreply.github.com>
2023-11-08 13:45:48 +0300

    Update ADC_REGISTERS.h

--- ASSESSMENT#2/MCAL/ADC_REGISTERS.h
+++ ASSESSMENT#2/MCAL/ADC_REGISTERS.h
@@ -1,1 +1,16 @@
+#ifndef _ADC_REGISTERS_H_
+#define _ADC_REGISTERS_H_
 
+
+
+
+#define   ADMUX_REG                 (*((volatile u8*)0X7C))
+#define   ADCSRA_REG                (*((volatile u8*)0X7A))
+#define   ADCH_REG                  (*((volatile u8*)0X79))
+#define   ADCL_REG                  (*((volatile u8*)0X78))
+#define   ADC_DATA                  (*((volatile u16*)0X78))
+
+
+
+
+#endif

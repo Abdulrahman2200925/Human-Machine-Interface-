0baaab0f6c6ce702b607a22c74324ed62c00699e
Amr5304 <114486804+Amr5304@users.noreply.github.com>
2023-11-08 13:16:47 +0200

    Add files via upload

--- ASSESSMENT#2/LIB/BIT.h
+++ ASSESSMENT#2/LIB/BIT.h
@@ -1,1 +1,22 @@
+#ifndef _BIT_H_
+#define _BIT_H_
+
+
+
+#define SET_BIT(REG,BIT_NUM)    REG|=(1<<BIT_NUM)
+#define CLR_BIT(REG,BIT_NUM)    REG&=(~(1<<BIT_NUM))
+#define GET_BIT(REG,BIT_NUM)    ((REG>>BIT_NUM)&1)
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
 

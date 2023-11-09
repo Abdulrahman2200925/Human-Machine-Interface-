52ed3005bea5cc191e1e63c8c1e54c7e970582fa
Abdulrahman2200925 <114488000+Abdulrahman2200925@users.noreply.github.com>
2023-11-09 02:07:45 +0200

    Update LM35.ino

--- ASSESSMENT#2/HAL/LM35.ino
+++ ASSESSMENT#2/HAL/LM35.ino
@@ -9,7 +9,7 @@
 
 u8 ADCToTemperature(u16 adcReading) {
 	u16 analog_value = (u16)(((u32)adcReading *(u32) 5000UL) / 1024UL); // Assuming V_REF is 5V   // To Convert from ( V --> mV )
-   // u16 analog_value = (u16)(((u32)adcReading *(u32) 1100UL) / 1024UL); // Assuming V_REF is 1.1V   // To Convert from ( V --> mV )
+   // u16 analog_value = (u16)(((u32)adcReading *(u32) 1100UL) / 1024UL); // Assuming INTERNAL V_REF  1.1V   // To Convert from ( V --> mV )
 	u8 temperatureC = (analog_value / 10); // LM35 sensitivity is 10 mV per 1°C
 	return temperatureC;
 	
@@ -20,21 +20,21 @@ void DisplayTemperatureOnLCD(u8 temperature) {
 	 
    
 	// Display temperature label
-	LCD_voidSetPosition(1, 1);
-	LCD_voidSendString("Temp:");
+	I2CLCD_voidSetPosition(1, 1);
+	I2CLCD_voidSendString("Temp:");
 
 	// Position the cursor for temperature value
-	LCD_voidSetPosition(1, 8);
+	I2CLCD_voidSetPosition(1, 8);
 
 	// Display temperature value as a two-digit number
 
-	displayNumberWithLeadingZeros(temperature);
+	 I2CdisplayNumberWithLeadingZeros(temperature);
 
 	// Position the cursor for temperature unit 'C'
-	LCD_voidSetPosition(1, 11);
+	I2CLCD_voidSetPosition(1, 11);
 
 	// Display temperature unit
-	LCD_voidSendString("  C'");
+	I2CLCD_voidSendString("  C'");
    
 	
 }
